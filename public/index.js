e9dc1706bbd53b0ba0f64fdaa9efc1290375ca34
abraxasrex <jbreasor@gmail.com>
2016-07-28 16:45:42 -0700

    chat messages reintegrated

--- public/index.js
+++ public/index.js
@@ -8,12 +8,42 @@ $('form').submit(function(){
   return false;
 });
 
-socket.on('chat message', function(msg){
+socket.on('chat message', function(msg) {
     $('#messages').append($('<li>').text(msg));
+    if (msg == "play") {
+        synth = flock.synth({
+            synthDef: {
+                id: 'carrier',
+                ugen: "flock.ugen.sinOsc",
+                inputs: {
+                  freq: 440
+                },
+                mul: {
+                    ugen: "flock.ugen.asr",
+                    start: 0.0,
+                    attack: 0.25,
+                    sustain: 0.25,
+                    release: 1.0,
+                    gate: {
+                        ugen: "flock.ugen.impulse",
+                        rate: "control",
+                        freq: 0.75,
+                        phase: 1.0
+                    }
+                }
+            }
+        });
+        flock.enviro.shared.play();
+    } else if (msg == "stop") {
+      io.emit('chat message', 'got it');
+        flock.enviro.shared.stop();
+    }
 });
 
-socket.on('sendSynth', function(msg){
-  synth = flock.synth({ synthDef:msg });
+socket.on('midi', function(freq){
+    $('#messages').append($('<li>').text(freq));
+    synth.set("carrier.freq", freq);
+
 });
 
 
--- server/server.js
+++ server/server.js
@@ -15,33 +15,9 @@ app.use(express.static(path.join(__dirname, '../public')));
 io.on('connection', function(socket) {
     socket.broadcast.emit('hi');
     socket.on('chat message', function(msg) {
+          //  $('#messages').append($('<li>').text(msg));
         io.emit('chat message', msg);
-        if (msg == "play") {
-            var synth = flock.synth({
-                synthDef: {
-                    ugen: "flock.ugen.sinOsc",
-                    freq: 440,
-                    mul: {
-                        ugen: "flock.ugen.asr",
-                        start: 0.0,
-                        attack: 0.25,
-                        sustain: 0.25,
-                        release: 1.0,
-                        gate: {
-                            ugen: "flock.ugen.impulse",
-                            rate: "control",
-                            freq: 0.75,
-                            phase: 1.0
-                        }
-                    }
-                }
-            });
-            flock.enviro.shared.play();
-        } else if (msg == "stop") {
-          io.emit('chat message', 'got it');
-            flock.enviro.shared.stop();
-        }
-    });
+      });
     socket.on('disconnect', function() {
         console.log('user disconnected');
     });
