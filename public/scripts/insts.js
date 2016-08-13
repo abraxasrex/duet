43a1047872b038baa08e41439a578e2ea69743a0
abraxasrex <jbreasor@gmail.com>
2016-08-12 17:05:22 -0700

    added granulator and moog synthesizers

--- public/scripts/insts.js
+++ public/scripts/insts.js
@@ -51,5 +51,45 @@ var instrumentDefs = {
             ugen: "flock.ugen.tri",
             freq: 440,
         }
+    },
+    moog: {
+    synthDef: {
+      id:'mod3',
+      ugen: "flock.ugen.filter.moog",
+      cutoff: {
+             id:'mod1',
+             ugen: "flock.ugen.sinOsc",
+             freq: 1/4,
+             mul: 5000,
+             add: 7000
+         },
+      resonance: {
+             id:'mod2',
+             ugen: "flock.ugen.sinOsc",
+             freq: 1/2,
+             mul: 1.5,
+             add: 1.5
+         },
+      source: {
+             ugen: "flock.ugen.lfSaw",
+             id:"carrier"
+         },
+      mul: 0.5
     }
+  },
+  granny: {
+    synthDef: {
+      id:'gran',
+      ugen: "flock.ugen.granulator",
+      numGrains: 20,
+      grainDur: 0.5,
+      delayDur: 1/4,
+      mul: 0.5,
+      source: {
+        id:'carrier',
+          ugen: "flock.ugen.lfSaw",
+          mul: 0.5
+      }
+    }
+  }
 }
